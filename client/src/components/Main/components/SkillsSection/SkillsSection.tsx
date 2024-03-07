b8cc80c4d5e3932be70d0062bfa6c93c5fde7877
Victor <vidrums86@gmail.com>
2024-03-07 21:41:17 +0000

    styling process...

--- client/src/components/Main/components/SkillsSection/SkillsSection.tsx
+++ client/src/components/Main/components/SkillsSection/SkillsSection.tsx
@@ -5,25 +5,26 @@ const SkillsSection = () => {
     <section className="skills-section">
       <div className="global-container">
         <div className="skills-wrapp">
+          
           <div className="skills-item">
             <h3 className="skill">Javascript</h3>
             <p className="experience">4 years experience</p>
           </div>
           <div className="skills-item">
             <h3 className="skill">Sass</h3>
-            <p className="experience">4 years experience</p>
+            <p className="experience">3 years experience</p>
           </div>
           <div className="skills-item">
             <h3 className="skill">Gulp</h3>
-            <p className="experience">4 years experience</p>
+            <p className="experience">2 years experience</p>
           </div>
           <div className="skills-item">
             <h3 className="skill">React</h3>
-            <p className="experience">4 years experience</p>
+            <p className="experience">2 years experience</p>
           </div>
           <div className="skills-item">
             <h3 className="skill">Node</h3>
-            <p className="experience">4 years experience</p>
+            <p className="experience">2 years experience</p>
           </div>
           <div className="skills-item">
             <h3 className="skill">Git</h3>
