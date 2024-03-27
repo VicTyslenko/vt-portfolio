c4050bbefc8721a1e6413af867cf9ad8b83884d0
Victor <vidrums86@gmail.com>
2024-03-27 19:04:37 +0000

    deploy...

--- client/src/config/API.js
+++ client/src/config/API.js
@@ -1,5 +1,4 @@
-// export const API_URL = process.env.REACT_APP_API_URL;
 export const API_URL =
   process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_API_URL // URL для локальной разработки
-    : process.env.REACT_APP_API_URL_PROD; // URL для продакшена
+    ? process.env.REACT_APP_API_URL
+    : process.env.REACT_APP_API_URL_PROD;
