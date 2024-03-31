cca47e843ccc0086bc1983a9a99d681160fec8e3
Victor <vidrums86@gmail.com>
2024-03-31 13:09:15 +0100

    testing HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
@@ -6,9 +6,9 @@ import store from "./store";
 import "./styles/global.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
