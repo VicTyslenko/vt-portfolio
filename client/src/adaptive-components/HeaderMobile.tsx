c67c41858d983cf7fea7e9019147eb8e80cd2f24
Victor <vidrums86@gmail.com>
2024-04-18 20:01:12 +0100

    started adaptive

---
+++ client/src/adaptive-components/HeaderMobile.tsx
@@ -0,0 +1,17 @@
+// import MenuIcon from '@mui/icons-material/Menu';
+
+interface HeaderMobileProps {
+  title: string;
+  className: string;
+  onToggleMenu: () => void;
+}
+
+const HeaderMobile: React.FC<HeaderMobileProps> = ({
+  title,
+  className,
+  onToggleMenu,
+}) => {
+  return <div>HeaderMobile</div>;
+};
+
+export default HeaderMobile;
