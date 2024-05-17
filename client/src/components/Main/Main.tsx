b38ea9188751093952937ac5d3ca1136fa95a258
Victor <vidrums86@gmail.com>
2024-05-17 17:16:27 +0100

    removed console.log

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,19 +1,8 @@
-import {
-  MainSection,
-  SkillsSection,
-  ProjectsSection,
-  FooterSection,
-} from "./components";
+import { MainSection, SkillsSection, ProjectsSection, FooterSection } from "./components";
 
-import { useEffect } from "react";
 import "../../styles/global.scss";
 
 const Main: React.FC = () => {
-
-  useEffect(()=>{
-console.log('hello');
-
-  },[])
   return (
     <div
       style={{
