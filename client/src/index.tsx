1a06cd7af5ec0a8b0f78cdc7c7ae7c208228d858
Victor Tyslenko <vidrums86@gmail.com>
2026-03-04 20:09:11 +0000

    fix: rename index.jsx to index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./styles/global.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
